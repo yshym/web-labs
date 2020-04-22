38b1fd28a95a1f7eb5da821215efad342349d224
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-04-22 04:45:08 +0300

    Add lab 5

---
+++ 5/http.js
@@ -0,0 +1,36 @@
+function httpGet(url, onprogress, onload) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+
+        xhr.onload = function () {
+            if (this.status === 200) {
+                resolve(this.response);
+                onload && onload();
+            } else {
+                let error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(new Error("Network Error"));
+        };
+
+        if (onprogress) {
+            xhr.onprogress = onprogress;
+        }
+
+        xhr.send();
+    });
+}
+
+export function fetchAvatars(n, onprogress, onload) {
+    return httpGet(`https://randomuser.me/api/?results=${n}`, onprogress, onload)
+        .then((response) => {
+            let users = JSON.parse(response).results;
+
+            return users.map((user) => user.picture);
+        });
+}
---
+++ 5/index.js
@@ -0,0 +1,65 @@
+import { fetchAvatars } from "./http.js";
+
+const avatarsDiv = document.getElementById("avatars");
+
+function addAvatarsRow(links, container) {
+    let avatarsRowDiv = document.createElement("div");
+    avatarsRowDiv.classList.add("avatars__row");
+
+    links.forEach((link) => {
+        let avatarImg = document.createElement("img");
+        avatarImg.src = link;
+
+        avatarsRowDiv.appendChild(avatarImg);
+    });
+
+    container.appendChild(avatarsRowDiv);
+    container.appendChild(document.createElement("br"));
+}
+
+function addAvatars(links, container) {
+    while (links.length >= 5) {
+        addAvatarsRow(links.splice(0, 5), container);
+    }
+    addAvatarsRow(links, container);
+}
+
+let loadingH1 = document.createElement("h1");
+loadingH1.appendChild(document.createTextNode("Loading..."));
+loadingH1.classList.add("loading");
+
+fetchAvatars(50).then((avatars) => {
+    addAvatars(
+        avatars.map((avatar) => avatar.large),
+        avatarsDiv
+    );
+});
+
+function getDocHeight() {
+    return Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+    );
+}
+
+console.log(`Doc height: ${getDocHeight()}`);
+console.log(`Inner height: ${window.innerHeight}`);
+
+window.onscroll = function () {
+    if (window.innerHeight + window.pageYOffset >= getDocHeight()) {
+        fetchAvatars(
+            25,
+            () => document.body.appendChild(loadingH1),
+            () => document.body.removeChild(loadingH1),
+        ).then((avatars) => {
+            addAvatars(
+                avatars.map((avatar) => avatar.large),
+                avatarsDiv
+            );
+        });
+    }
+};
